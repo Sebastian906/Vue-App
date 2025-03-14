478f823193e8d11868b9d1749e9f62763d64d6fd
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-03-13 22:36:06 -0500

    Comentarios para explicar propiedades presentes en las clases .vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
-<script setup>
+<script setup> // setup: Método que permite usar el script setup proporcionado por Vue 3
   const msg = 'Hello Vue 3 + Vite';
   function saludar() {
     console.log('Hello');
   }
 </script>
 
-<template>
+<template> <!-- template: Contiene el HTML que se renderizará en el navegador -->
   <h1>
     {{ msg }}
   </h1>
 </template>
 
-<style scoped>
+<style scoped> /* scoped: Permite que los estilos solo se apliquen al componente actual */
 
-</style>
+</style> 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-//import './assets/main.css'
+//import './assets/main.css' // importa la hoja de estilos css denominada como main.css
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue' // importa la función de createApp para hacer la aplicación desde 0
+import App from './App.vue' // importa el componente App.vue
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') // crea la aplicación y la monta en el div con el id correspondiente (en este caso id="app")
