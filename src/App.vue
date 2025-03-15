7c76ecff114df75527ca2e4af57b84473418c8cd
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-03-14 23:02:46 -0500

    Uso de interpolaciones con v-html y v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script setup> // setup: Método que permite usar el script setup proporcionado por Vue 3
-  const msg = 'Hello Vue 3 + Vite';
-  function saludar() {
-    console.log('Hello');
+  const atributos = {
+    id: 'idPrueba',
+    class: 'text-red',
+    disabled: true
   }
 </script>
 
 <template> <!-- template: Contiene el HTML que se renderizará en el navegador -->
-  <h1>
-    {{ msg }}
-  </h1>
+  <!-- <div v-html="msg"></div> -->
+  <p v-bind="atributos">
+    Hola Mundo
+  </p>
+  <button v-bind="atributos">
+    Enviar
+  </button>
 </template>
 
 <style scoped> /* scoped: Permite que los estilos solo se apliquen al componente actual */
-
+  .text-red {
+    color: red;
+  }
 </style> 
