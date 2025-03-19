96602b14f0429b0073345295cd4b11c207cda089
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-03-18 22:01:00 -0500

    Se emplea un ejemplo de conteo dentro de la aplicación y se muestra la funcionalidad reactiva de la propiedad ref

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
-<script setup> // setup: Método que permite usar el script setup proporcionado por Vue 3
-  const atributos = {
-    id: 'idPrueba',
-    class: 'text-red',
-    disabled: true
+<script setup>
+import { ref } from 'vue';
+
+ // setup: Método que permite usar el script setup proporcionado por Vue 3
+  let count = ref(0); // ref: Permite crear una variable reactiva
+  
+  function decrement() {
+    count.value--;
+  }
+  
+  function increment() {
+    count.value++;
   }
 </script>
 
 <template> <!-- template: Contiene el HTML que se renderizará en el navegador -->
   <!-- <div v-html="msg"></div> -->
-  <p v-bind="atributos">
-    Hola Mundo
-  </p>
-  <button v-bind="atributos">
-    Enviar
+  <button @click="decrement()" :disabled="count === 0"> <!-- @click: Permite ejecutar una función al hacer clic en el elemento. Se deshabilita la función cuando el valor es 0 -->
+    - 
+  </button>
+  
+  {{ count }}
+  
+  <button @click="increment()"> 
+    + 
   </button>
 </template>
 
 <style scoped> /* scoped: Permite que los estilos solo se apliquen al componente actual */
-  .text-red {
-    color: red;
-  }
+
 </style> 
